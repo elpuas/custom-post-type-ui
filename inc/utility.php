885e3f1f5bb2b4617197a51e29d20d1da11077fd
Michael Beckwith <tw2113@gmail.com>
2016-08-18 18:39:43 -0500

    helper functions to retrieve single objects out of CPTUI settings

--- inc/utility.php
+++ inc/utility.php
@@ -635,3 +635,37 @@ function cptui_error_admin_notice() {
 		false
 	);
 }
+
+/**
+ * Returns saved values for single post type from CPTUI settings.
+ *
+ * @since 1.5.0
+ *
+ * @param string $post_type Post type to retrieve CPTUI object for.
+ * @return string
+ */
+function cptui_get_cptui_post_type_object( $post_type = '' ) {
+	$post_types = get_option( 'cptui_post_types' );
+
+	if ( array_key_exists( $post_type, $post_types ) ) {
+		return $post_types[ $post_type ];
+	}
+	return '';
+}
+
+/**
+ * Returns saved values for single taxonomy from CPTUI settings.
+ *
+ * @since 1.5.0
+ *
+ * @param string $taxonomy Taxonomy to retrieve CPTUI object for.
+ * @return string
+ */
+function cptui_get_cptui_taxonomy_object( $taxonomy = '' ) {
+	$taxonomies = get_option( 'cptui_taxonomies' );
+
+	if ( array_key_exists( $taxonomy, $taxonomies ) ) {
+		return $taxonomies[ $taxonomy ];
+	}
+	return '';
+}
